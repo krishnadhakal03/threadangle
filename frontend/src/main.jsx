41f8bcd3d905a70fabce3af0637561b2e6d0fcfa
krishnadhakal03 <krishna.dhakal03@gmail.com>
2026-05-02 12:37:28 -0400

    Wire text input key guard into frontend entrypoint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './utils/preserveTextInputKeys.js'
 import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
